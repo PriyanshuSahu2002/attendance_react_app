f42167b2c9fc7114f34f122bf924e9e691d781be
Xcoder <jaykumartandan6@gmail.com>
2023-01-23 22:12:12 +0530

    sidebar update
    sidebaritems  half updated

--- src/Components/SideBarItems.jsx
+++ src/Components/SideBarItems.jsx
@@ -1,51 +1,24 @@
 import React from 'react'
 import styled from 'styled-components'
-import {DiDatabase} from 'react-icons/di' 
-import {BiLibrary} from 'react-icons/bi'
-import {FcDocument} from 'react-icons/fc'
+
 
 const Container = styled.div`
-`
-const Box1 = styled.div`
 display:flex;
 justify-content: left;
 margin-left: 10px;
-box-shadow: 0px 3px 3px rgba(0, 0, 0, 2%);
-height: 35px;
+// box-shadow: 0px 3px 3px rgba(0, 0, 0, 2%);
 `
 const Icon = styled.i`
 `
 const Label = styled.span`
 margin-left:10px;
 `
-const H5 = styled.h5`
-margin: 30px 0px 0px 10px;
-color: darkgray;
-`
 
-const Box2 = styled.div`
-    display: grid;
-    grid-template-columns: 1fr 9fr;
-    row-gap: 20px;
-    margin: 20px 0px 0px 5px;
-    box-shadow: 0px 3px 3px rgba(0, 0, 0, 2%);
-    height:90px;
-`
-
-const SideBarItems = () => {
+const SideBarItems = (props) => {
   return (
     <Container>
-      <Box1>
-        <Icon><DiDatabase style={{color: 'mediumslateblue', fontSize: '23px'}} /></Icon>
-        <Label> DashBoard</Label>
-      </Box1>
-      <H5>Test</H5>
-      <Box2>
-        <Icon><BiLibrary style={{color: 'mediumslateblue', fontSize: '23px'}} /></Icon>
-        <Label> Items</Label>
-        <Icon><FcDocument style={{fontSize: '23px'}} /></Icon>
-        <Label> Exams</Label>
-        </Box2>
+        <Icon>{props.icon}</Icon>
+        <Label> {props.name}</Label>
     </Container>
   )
 }
--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import styled from 'styled-components'
 import SideBarItems from './SideBarItems'
-
+import {DiDatabase} from 'react-icons/di'
+import {FcDocument} from 'react-icons/fc'
+import {BiLibrary} from 'react-icons/bi'
+import {MdLibraryBooks} from 'react-icons/md'
+import {MdSubscriptions} from 'react-icons/md'
+import {BsFillPersonFill} from 'react-icons/bs'
+import {HiUserGroup} from 'react-icons/hi'
+import {FaUserGraduate} from 'react-icons/fa'
+import {RiUserSettingsLine} from 'react-icons/ri'
 
 const Container = styled.div`
     float: left;
-
     width: 250px;
     height: ${props=>props.sizeY}px;
     background-color: white;
@@ -17,10 +24,23 @@ const Container = styled.div`
     left: -15px ;
     box-shadow: 0px 6px 10px rgba(0, 0, 0, 0.2);
 `
-const Sidebar = () => {
+const Sidebar = () =>{
+ let styling = {color: '#b7a1a1', marginLeft: '10px'};
+
   return (
     <Container sizeY = {window.innerHeight}>
-        <SideBarItems/>
+        <SideBarItems name = "Dashboard" icon = {<DiDatabase style={{fontSize: '23px'}} />} />
+        <h5 style = {styling} >Test</h5>
+        <SideBarItems name = "Items" icon = {<BiLibrary style={{fontSize: '23px'}} />} />
+        <SideBarItems name = "Exams" icon = {<FcDocument style={{fontSize: '23px'}} />} />
+        <h5 style = {styling}>Organize</h5>
+        <SideBarItems name = "Courses" icon = {<MdLibraryBooks style={{fontSize: '20px'}} />} />
+        <SideBarItems name = "Courses Subscription" icon = {<MdSubscriptions style={{fontSize: '20px'}} />} />
+        <SideBarItems name = "Students" icon = {<FaUserGraduate style={{fontSize: '20px'}} />} />
+        <SideBarItems name = "Private Groups" icon = {<HiUserGroup style={{fontSize: '20px'}} />} />
+        <h5 style = {styling}>Manage</h5>
+        <SideBarItems name = "User" icon = {<BsFillPersonFill style={{fontSize: '23px'}} />} />
+        <SideBarItems name = "Configuration" icon = {<RiUserSettingsLine style={{fontSize: '23px'}} />} />
     </Container>
   )
 }

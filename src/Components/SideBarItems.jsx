400500f046ded4f4a16d845e13f1e38b50a38a34
Priyanshu <sahupriyanshu108@gmail.com>
2023-01-28 11:32:30 +0530

    Added Styling to SideBar

--- src/Components/SideBarItems.jsx
+++ src/Components/SideBarItems.jsx
@@ -8,10 +8,14 @@ justify-content: left;
 margin-left: 10px;
 box-shadow: 0px 3px 3px rgba(0, 0, 0, 2%);
 transition-duration: 0.3s;
+margin: 20px 20px;
 height:30px;
+padding: 5px;
+align-items: center;
 &:hover{
-  box-shadow: 0 6px 6px 0 rgba(0,0,0,0.10);
+  box-shadow: 0 6px 6px 0 #00000019;
   cursor:pointer;
+  background-color:#4e4e4e ;
 }
 `
 const Icon = styled.i`
--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -4,7 +4,7 @@ import SideBarItems from './SideBarItems'
 import {AiTwotoneDashboard} from 'react-icons/ai'
 import {FaFileAlt} from 'react-icons/fa'
 import {BiLibrary} from 'react-icons/bi'
-import {MdLibraryBooks} from 'react-icons/md'
+import {MdDashboard, MdLibraryBooks} from 'react-icons/md'
 import {MdSubscriptions} from 'react-icons/md'
 import {BsFillPersonFill} from 'react-icons/bs'
 import {HiUserGroup} from 'react-icons/hi'
@@ -24,21 +24,26 @@ const Container = styled.div`
     left: -15px ;
     box-shadow: 0px 6px 10px rgba(0, 0, 0, 0.2);
 `
+const Label = styled.h5`
+  color:#6e6e6e ;
+  
+  margin: 20px 10px;
+`
 const Sidebar = () =>{
  let styling = {color: '#b7a1a1', marginLeft: '10px'};
- let fontSize = {fontSize: '20px'};
+ let fontSize = {fontSize: '20px' ,color:'#777777'};
   return (
     <Container sizeY = {window.innerHeight}>
-        <SideBarItems name = "Dashboard" icon = {<AiTwotoneDashboard style={fontSize}/>} />
-        <h5 style = {styling} >Test</h5>
+        <SideBarItems name = "Dashboard" icon = {<MdDashboard style={fontSize}/>} />
+        <Label>Test</Label>
         <SideBarItems name = "Items" icon = {<BiLibrary style={fontSize} />} />
         <SideBarItems name = "Exams" icon = {<FaFileAlt style={fontSize} />} />
-        <h5 style = {styling}>Organize</h5>
+        <Label>Organize</Label>
         <SideBarItems name = "Courses" icon = {<MdLibraryBooks style={fontSize} />} />
         <SideBarItems name = "Courses Subscription" icon = {<MdSubscriptions style={fontSize} />} />
         <SideBarItems name = "Students" icon = {<FaUserGraduate style={fontSize} />} />
         <SideBarItems name = "Private Groups" icon = {<HiUserGroup style={fontSize} />} />
-        <h5 style = {styling}>Manage</h5>
+        <Label>Manage</Label>
         <SideBarItems name = "User" icon = {<BsFillPersonFill style={fontSize} />} />
         <SideBarItems name = "Configuration" icon = {<RiUserSettingsLine style={fontSize} />} />
     </Container>
